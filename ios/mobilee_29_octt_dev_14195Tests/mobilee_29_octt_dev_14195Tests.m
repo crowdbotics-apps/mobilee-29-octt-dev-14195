a0f19d5187b3c83a88f6f8824d84d55d45200065
Crowdbotics <team@crowdbotics.com>
2020-10-29 06:23:32 +0000

    Initial commit.

---
+++ ios/mobilee_29_octt_dev_14195Tests/mobilee_29_octt_dev_14195Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface mobilee_29_octt_dev_14195 : XCTestCase
+
+@end
+
+@implementation mobilee_29_octt_dev_14195
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
